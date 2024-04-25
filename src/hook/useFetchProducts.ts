5a940b9d7037590f48c038891ac2b329fdeeb502
Jaman-dedy <jeandedieuam@gmail.com>
2024-04-25 19:38:09 +0200

    add .en vile

--- src/hook/useFetchProducts.ts
+++ src/hook/useFetchProducts.ts
@@ -2,12 +2,14 @@ import { useState, useCallback } from 'react';
 import { Product } from '../types/product';
 
 
+const URI:string = process.env.REACT_APP_API_URL || 'https://dummyjson.com/products'
 const useFetchProducts = () => {
+  console.log('URI :>> ', URI);
   const [data, setData] = useState<Product[]>([]);
 
   const fetchData = useCallback(async () => {
     try {
-      const response = await fetch('https://dummyjson.com/products');
+      const response = await fetch(URI);
       const jsonData = await response.json();
       if (jsonData.products && Array.isArray(jsonData.products)) {
         setData(jsonData.products);
